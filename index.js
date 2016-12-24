179b4bdc51987c39db5009313eaa6dd73ee9e3c5
Mateus Pinheiro <mateusp@br.ibm.com>
2016-12-24 00:53:19 -0200

    First commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const passport     = require('passport');
+const ldapStrategy = require('passport-ldapauth');
+
+// Setup passport with IBM's LDAP config
+const config = { server: {
+    url: 'ldaps://bluepages.ibm.com:636',
+    searchBase: 'ou=bluepages,o=ibm.com',
+    searchFilter: '(mail={{username}})',
+    searchAttributes: ['cn', 'uid', 'mail']
+}}
+passport.use(new ldapStrategy(config))
+passport.initialize()
+
+module.exports = function(req, res, next) {
+    passport.authenticate('ldapauth', {session: false}, function(err, user, info) {
+		if (err || !user)
+        	return res.sendStatus(401)
+      	
+      	// Sometimes user and mail can be arrays. Choose the first option
+      	// instead and make it a string
+        if (Array.isArray(user.cn) && user.cn.length > 0)
+			user.cn = user.cn[0]
+        if (Array.isArray(user.mail) && user.mail.length > 0)
+			user.mail = user.mail[0]
+
+		req.user = user
+		next();
+    })(req, res, next);
+  }
+}
+
